058a513be239d37d31f855b448a7f4d86128a56e
Matthias Lei <107970378+Matthias-Lei@users.noreply.github.com>
2026-02-26 23:05:15 +1100

    Add backend solutions

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass
 from typing import List, Dict, Union
 from flask import Flask, request, jsonify
 import re
+import json
+
+import logging
 
 # ==== Type Definitions, feel free to add or modify ===========================
 @dataclass
@@ -28,7 +31,8 @@ class Ingredient(CookbookEntry):
 app = Flask(__name__)
 
 # Store your recipes here!
-cookbook = None
+# cookbook = None
+cookbook = {"recipes": {}, "ingredients": {}, "entries": []}
 
 # Task 1 helper (don't touch)
 @app.route("/parse", methods=['POST'])
@@ -43,22 +47,68 @@ def parse():
 # [TASK 1] ====================================================================
 # Takes in a recipeName and returns it in a form that 
 def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: implement me
-	return recipeName
+	recipeName = recipeName.replace("-", " ")
+	recipeName = recipeName.replace("_", " ")
+	recipeName = re.sub("[^A-Za-z ]", "", recipeName).title()
+	recipeName = re.sub(" {2,}", " ", recipeName.strip())
+	if len(recipeName) > 0:
+		return recipeName
+	else:
+		return None
 
+def validate_cookbook_entry(entryDict: dict) -> bool:
+	entryType = entryDict["type"]
+	entryName = entryDict["name"]
+
+	if entryName in cookbook["entries"]:
+		return False
+	if entryType not in ("recipe", "ingredient"):
+		return False
+
+	if entryType == "recipe":
+		recipeItems = entryDict["requiredItems"]
+		ingredients = [item["name"] for item in recipeItems]
+		if len(set(ingredients)) != len(ingredients):
+			return False
+		ingredients = [RequiredItem(item["name"], item["quantity"]) for item in recipeItems]
+		# cookbook["recipes"][entryName] = recipeItems
+		cookbook["recipes"][entryName] = Recipe(name=entryName, required_items=ingredients)
+	else:
+		if entryDict["cookTime"] < 0:
+			return False
+		# cookbook["ingredients"][entryName] = entryDict["cookTime"]
+		cookbook["ingredients"][entryName] = Ingredient(name=entryName, cook_time=entryDict["cookTime"])
+
+
+	cookbook["entries"].append(entryName)
+	return True
 
 # [TASK 2] ====================================================================
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
+	if validate_cookbook_entry(request.json):
+		return "", 200
+	else:
+		return "", 400
 	# TODO: implement me
-	return 'not implemented', 500
+	# return 'not implemented', 500
 
 
 # [TASK 3] ====================================================================
 # Endpoint that returns a summary of a recipe that corresponds to a query name
 @app.route('/summary', methods=['GET'])
 def summary():
+	recipeName = request.args["name"]
+	try:
+		recipe = cookbook["recipes"][recipeName]
+		required_items = recipe.required_items
+		cooking_time = sum([cookbook["ingredients"][item.name].cook_time * item.quantity for item in required_items])
+		return jsonify({"name": recipeName, "cookTime": cooking_time, "ingredients": required_items}), 200
+	except KeyError:
+		return "", 400
+	
+	
 	# TODO: implement me
 	return 'not implemented', 500
 
